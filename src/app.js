8f4d52d3c2d89591fdc6ec0b242f09b41cece000
rodrigopavezi <rodrigopavezi@gmail.com>
2017-12-27 15:36:21 -0200

    Fix cors

--- src/app.js
+++ src/app.js
@@ -60,7 +60,7 @@ app.use((err, req, res, next) => {
 
 // CORS allow every origin as it requires user authorization
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization');
     res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
     res.header("Access-Control-Allow-Credentials", "true");
